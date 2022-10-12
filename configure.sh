52325ca91de1c7bc442361e97995394c62c82e4b
AlloLab <allolab@sphere>
2022-10-11 17:21:56 -0700

    sphere config

--- configure.sh
+++ configure.sh
@@ -5,15 +5,15 @@
   cd build
   mkdir -p release
   cd release
-  cmake -DCMAKE_BUILD_TYPE=Release -Wno-deprecated -DBUILD_EXAMPLES=0 ../..
+  /alloshare/cmake-3.15.3-Linux-x86_64/bin/cmake -DCMAKE_BUILD_TYPE=Release -Wno-deprecated -DBUILD_EXAMPLES=0 ../..
 )
 
 # Configure debug build
-(
-  mkdir -p build
-  cd build
-  mkdir -p debug
-  cd debug
-  cmake -DCMAKE_BUILD_TYPE=Debug -Wno-deprecated -DBUILD_EXAMPLES=0 ../..
-)
+#(
+#  mkdir -p build
+#  cd build
+#  mkdir -p debug
+#  cd debug
+#  cmake -DCMAKE_BUILD_TYPE=Debug -Wno-deprecated -DBUILD_EXAMPLES=0 ../..
+#)
 
