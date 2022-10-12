67a198485347f9002fdd58e7e9f4373c9b882914
ben <myunginlee@umail.ucsb.edu>
2022-10-11 17:34:51 -0700

    config

--- t_configure.sh
+++ t_configure.sh
@@ -13,7 +13,7 @@
   # utilizing cmake's parallel build options
   # recommended: -j <number of processor cores + 1>
   # This is supported in cmake >= 3.12 use -- -j5 for older versions
-  /alloshare/cmake-3.15.3-Linux-x86_64/bin/cmake --build build/release -j5
+  cmake --build build/release -j5
 )
 # # Configure debug build
 # (
